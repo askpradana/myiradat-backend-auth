1057ac55fecfe8f1704970cdd837fd1cc9f3b29f
naufal <askpradana@gmail.com>
2025-06-30 09:46:54 +0700

    deploy: basic running auth-service with docker

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World with dev-test!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Set JSON content type
+		w.Header().Set("Content-Type", "application/json")
+
+		// Create a simple response
+		resp := map[string]string{
+			"status": "auth service is running",
+		}
+
+		// Encode the response as JSON
+		json.NewEncoder(w).Encode(resp)
+	})
+
+	// Start the server on port 8080
+	if err := http.ListenAndServe(":7791", nil); err != nil {
+		panic(err)
+	}
 }
